7205b26f5f2a8cbf22ebfe427afe5f48b3bd6f71
Tisha1810 <itisha65@gmail.com>
2024-07-23 14:33:48 +0300

    FizzBuzz task

---
+++ src/FizzBuzz/FizzBuzzExample.java
@@ -0,0 +1,30 @@
+package FizzBuzz;
+
+public class FizzBuzzExample {
+    public static void main(String[] args) {
+        short[] array = new short[100];
+        fillArray(array);
+        fizzBuzzCheck(array);
+
+    }
+
+    public static void fillArray(short[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (short) (i + 1);
+        }
+    }
+
+    public static void fizzBuzzCheck(short[] array) {
+        for (short value : array) {
+            if (value % 3 == 0 & value % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (value % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (value % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(value);
+            }
+        }
+    }
+}
