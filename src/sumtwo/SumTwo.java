ac7efe1d971ef1926fc9c69f7026010ce625037e
Tisha1810 <itisha65@gmail.com>
2024-07-23 19:10:33 +0300

    SumTow task

---
+++ src/sumtwo/SumTwo.java
@@ -0,0 +1,43 @@
+package sumtwo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SumTwo {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        fillArray(array);
+        System.out.println(Arrays.toString(array));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input target:");
+        int target = scanner.nextInt();
+
+        int[] arr = new int[2];
+        arr = sumTwo(array, target);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void fillArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(1, 50);
+        }
+    }
+
+    public static int[] sumTwo(int[] array, int target) {
+        int firstIndex = 0;
+        int secondIndex = 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            firstIndex = i;
+            secondIndex = i + 1;
+            sum = array[i] + array[i + 1];
+            if (sum == target) {
+                break;
+            }
+        }
+        return new int[]{firstIndex, secondIndex};
+    }
+
+}
