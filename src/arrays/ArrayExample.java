8c76709a7319883e35aae13ed0ca4f9c249eaecb
Tisha1810 <itisha65@gmail.com>
2024-07-23 13:37:30 +0300

    Format code

--- src/arrays/ArrayExample.java
+++ src/arrays/ArrayExample.java
@@ -15,7 +15,6 @@ public class ArrayExample {
         System.out.println();
     }
 
-
     public static void sortChanger(Integer[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
